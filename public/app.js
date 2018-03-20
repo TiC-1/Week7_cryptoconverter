b475cf305fcf0362747e1f4ee2658b78a0983550
Orionlight231 <orionlight231@gmail.com>
2018-03-20 17:49:50 +0100

    first frontend

---
+++ public/app.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: '#field1',
+  data: {
+    selected: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' }
+    ]
+  }
+})
+
+new Vue({
+  el: '#field2',
+  data: {
+    selected: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' }
+    ]
+  }
+})
+
+new Vue({
+      el: '#textField',
+      data: {
+        message: ''
+      }
+    })
